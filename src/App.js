5d0f326baf9505e0180acffb81a024fd4a29dad7
Khilesh Thakur <omthakur6640@gmail.com>
2024-10-10 01:31:16 +0530

    Pushing BaseCode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { REACT_APP_API_URL } from './Url';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get(`${REACT_APP_API_URL}/api`)
+      .then(response => setData(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{data ? data.message : 'Loading...'}</h1>
     </div>
   );
 }
---
+++ src/Url.js
@@ -0,0 +1,1 @@
+export const REACT_APP_API_URL = `https://learnit-backend-2r64.onrender.com`
