ae04ceb8e05d71cd988896d2733cae63abc6772f
Khilesh Thakur <omthakur6640@gmail.com>
2024-10-10 03:13:40 +0530

    Base Code Frontend

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { REACT_APP_API_URL } from './Url';
 
 function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    axios.get(`${REACT_APP_API_URL}/api`)
+    axios.get(`${process.env.REACT_APP_API_URL}/api`)
       .then(response => setData(response.data))
       .catch(error => console.error(error));
   }, []);
-
+  
   return (
     <div>
       <h1>{data ? data.message : 'Loading...'}</h1>
--- src/Url.js
+++
@@ -1,1 +0,0 @@
-export const REACT_APP_API_URL = `https://learnit-backend-2r64.onrender.com`
