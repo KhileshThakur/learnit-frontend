52011601335069d2981171613b01105540d42657
khushiichaudhari <chaudharikhushi67@gmail.com>
2024-10-25 15:05:43 +0530

    Login page

--- src/App.js
+++ src/App.js
@@ -4,34 +4,22 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 //Import Components
 import Homepage from './Home/Pages/Homepage';
 import Login from './Auth/Login';
-import RegisterLearner from './Auth/RegisterLearner';
-import RegisterInstructor from './Auth/RegisterInstructor';
+// import Authenticate from './Auth/Components/Authenticate';
+import JoinAsLearner from './Auth/Components/JoinAsLearner';
+import JoinAsInstructor from './Auth/Components/JoinAsInstructor';
 
 
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div>
-
-      <h1>{data ? data.message : 'Loading...'}</h1>
-      <h2>Hello</h2>
-
-      <h1>{data ? data.message : 'Loading Data...'}</h1>
-      <h2>Changes Made By Khilesh Thakur Again Modified</h2>
-      <h3>Changes Made in Main</h3>
-      <h3>Changes made by Khushi</h3>
-    </div>
-=======
     <Router>
       <Routes>
         <Route path="/" element={<Homepage/>} />
         <Route path="/auth" element={<Login/>} />
-        <Route path="/register-learner" element={<RegisterLearner/>} />
-        <Route path="/register-instructor" element={<RegisterInstructor/>} />
+        <Route path="/register-learner" element={<JoinAsLearner/>} />
+        <Route path="/register-instructor" element={<JoinAsInstructor/>} />
       </Routes>
     </Router>
->>>>>>> b096582f85d5bdcffdfda22c41e0c0122f88f315
   );
 }
 
