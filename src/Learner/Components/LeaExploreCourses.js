17fad3d6c56f80c15488690260888a87c5a48b32
Khilesh Thakur <113234354+KhileshThakur@users.noreply.github.com>
2025-05-04 09:54:38 +0530

    Update LeaExploreCourses.js

--- src/Learner/Components/LeaExploreCourses.js
+++ src/Learner/Components/LeaExploreCourses.js
@@ -12,7 +12,7 @@ const LeaExploreCourses = ({ learnerId }) => {
   const params = useParams();
   // Use provided learnerId or get from URL params
   const learnerIdToUse = learnerId || params.id;
-  const backendUrl = process.env.REACT_APP_BACKEND_URL;
+  const backendUrl = process.env.REACT_APP_BACKEND;
   
   // Default course image
   const defaultCourseImage = 'https://via.placeholder.com/300x180?text=Course+Image';
